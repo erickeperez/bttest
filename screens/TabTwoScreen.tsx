a3788f7ba9c56c0e32933220b2e77f5a807846ff
Erick Perez <erickeperez24@gmail.com>
2020-09-15 18:05:41 -0400

    Update text msg

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -10,7 +10,7 @@ export default function TabTwoScreen() {
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-      <Text>Hello</Text>
+      <Text>Hello Again </Text>
     </View>
   );
 }
